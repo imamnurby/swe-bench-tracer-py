1c3c547ca188d4c4adc113419261af9bb2087eaf
imamnurby <imamnurby@gmail.com>
2025-09-19 14:46:28 +0800

    init file

---
+++ tests/test_slicer.py
@@ -0,0 +1,14 @@
+from slicer import execute_backward_slice_for_buggy_code
+import pprint as pp
+
+
+if __name__ == "__main__":
+    json_filepath = "/home/yusuf/ds-symbolic-explanation/swe-bench-tracer-py/tests/sample_programs/buggy.jsonl"
+    slice_result, starting_statement, starting_function_name, starting_filepath = execute_backward_slice_for_buggy_code(json_filepath, target_event_type="Exception")
+
+    for event in slice_result:
+        print(event)
+        pp.pprint(event)
+        
+    pp.pprint(f"Slicing started from the statement: {starting_statement} in function {starting_function_name} located at {starting_filepath}")
+    
