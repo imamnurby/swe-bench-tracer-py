281a13a9639e41fdfc867728f946423a1bd44b9c
imamnurby <imamnurby@gmail.com>
2025-09-19 15:41:48 +0800

    add assertion

--- tests/test_slicer_buggy.py
+++ tests/test_slicer_buggy.py
@@ -6,7 +6,11 @@ if __name__ == "__main__":
     json_filepath = "/home/yusuf/ds-symbolic-explanation/swe-bench-tracer-py/tests/sample_programs/buggy.jsonl"
     slice_result, starting_statement, starting_function_name, starting_filepath = execute_backward_slice_for_buggy_code(json_filepath, target_event_type="Exception")
 
-    for event in slice_result:
+    assert len(slice_result) == 4
+    expected_event_ids = (10, 4, 3, 2)
+    result_event_ids = []
+    for expected_event_ids, event in zip(expected_event_ids, slice_result):
+        assert(expected_event_ids == event.get("event_id"))
         pp.pprint(event)
         
     pp.pprint(f"Slicing started from the statement: {starting_statement} in function {starting_function_name} located at {starting_filepath}")
