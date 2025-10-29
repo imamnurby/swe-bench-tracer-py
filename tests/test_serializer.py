f00d2e023ec7f29e29f526c61721e9e7a2921813
Zhiyuan Pan <zy_pan@zju.edu.cn>
2025-10-29 16:04:33 +0800

    add serializer tests

--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -1,6 +1,10 @@
 import traceback
+import numpy as np
 
 from tracer.serializer import serialize, deserialize
+
+def assert_equals(obj, expected):
+    assert obj == expected, f"Provided: {obj}\nExpected: {expected}"
 
 def test_dict_of_funcs_in_class():
     def dummy_func():
@@ -13,7 +17,23 @@ def test_dict_of_funcs_in_class():
     
     data = A()
     serialized = serialize(data)
-    assert serialized == {'py/object': '__main__.test_dict_of_funcs_in_class.<locals>.A', 'data': {'key': {'py/function': '__main__.test_dict_of_funcs_in_class.<locals>.dummy_func'}}}
+    assert_equals(serialized, {'py/object': '__main__.test_dict_of_funcs_in_class.<locals>.A', 'data': {'key': {'py/function': '__main__.test_dict_of_funcs_in_class.<locals>.dummy_func'}}})
+
+def test_normal_registered_type():
+    arr = np.array([1, 2, 3])
+    serialized = serialize(arr)
+    assert_equals(serialized, {'py/object': 'numpy.ndarray', 'dtype': 'int64', 'values': [1, 2, 3]})
+    deserialized = deserialize(serialized)
+    assert_equals(arr.tolist(), deserialized.tolist())
+
+def test_subclass_of_registered_type():
+    class MyArray(np.ndarray):
+        value = 42
+    
+    obj = np.array([1, 2, 3]).view(MyArray)
+    serialized = serialize(obj)
+    
+    assert_equals(serialized, {'py/reduce': [{'py/function': 'numpy._core.multiarray._reconstruct'}, {'py/tuple': [{'py/type': '__main__.test_subclass_of_registered_type.<locals>.MyArray'}, {'py/tuple': [0]}, {'py/b64': 'Yg=='}]}, {'py/tuple': [1, {'py/tuple': [3]}, {'py/reduce': [{'py/type': 'numpy.dtype'}, {'py/tuple': ['i8', False, True]}, {'py/tuple': [3, '<', None, None, None, -1, -1, 0]}]}, False, {'py/b64': 'AQAAAAAAAAACAAAAAAAAAAMAAAAAAAAA'}]}]})
 
 if __name__ == "__main__":
     test_funcs = [obj for name, obj in globals().items() if name.startswith('test_') and callable(obj)]
