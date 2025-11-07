ca40a25408a5942cf8cbf9c4003b2fe9053d5dee
Zhiyuan Pan <zy_pan@zju.edu.cn>
2025-11-07 12:52:31 +0800

    compatibility for lower version Python

--- tracer/serializer/ext/__init__.py
+++ tracer/serializer/ext/__init__.py
@@ -8,7 +8,7 @@ def register_handlers():
     registered_types = []
     dir = os.path.abspath(os.path.dirname(__file__))
     for _, modname, _ in pkgutil.iter_modules([dir]):
-        module = importlib.import_module(f'tracer.serializer.ext.{modname}')
+        module = importlib.import_module('tracer.serializer.ext.{}'.format(modname))
         if hasattr(module, 'register_handlers'):
             registered_types.extend(module.register_handlers() or [])
     return registered_types
