87d4035c795b0b187047db83b6d72c5b4e63995b
imamnurby <imamnurby@gmail.com>
2025-12-18 13:16:41 +0800

    add more attributes to the httpResponsebase handler

--- tracer/serializer/ext/django.py
+++ tracer/serializer/ext/django.py
@@ -69,23 +69,48 @@ class DatabaseWrapperHandler(BaseHandler):
     def restore(self, obj):
         return obj
 
-class HttpResponseRedirectLocationHandler(BaseHandler):
+class DjangoHttpResponseHeadersHandler(BaseHandler):
+    HEADER_WHITELIST = (
+        "Content-Type",
+        "Content-Encoding",
+        "Location",
+        "Vary",
+    )
+
     def flatten(self, obj, data):
         result = {"py/object": canonical_class_name(obj)}
         try:
             result["status_code"] = getattr(obj, "status_code", None)
-            if hasattr(obj, "headers"):
-                loc = obj.headers.get("Location")
-                if loc is not None:
-                    result["location"] = str(loc)
-
-            # Older Django: response._headers (dict of lower->(orig,value))
-            elif hasattr(obj, "_headers"):
-                h = getattr(obj, "_headers", {})
-                if "location" in h and isinstance(h["location"], (tuple, list)) and len(h["location"]) == 2:
-                    result["location"] = str(h["location"][1])
         except Exception:
             pass
+
+        extracted = {}
+        try:
+            if hasattr(obj, "headers"):
+                # Django 2.2+ (Mapping-like, case-insensitive)
+                for key in self.HEADER_WHITELIST:
+                    val = obj.headers.get(key)
+                    if val is not None:
+                        extracted[key] = str(val)
+            elif hasattr(obj, "_headers"):
+                # Older style: {lower: (orig, value)}
+                h = getattr(obj, "_headers", {}) or {}
+                for key in self.HEADER_WHITELIST:
+                    lk = key.lower()
+                    if lk in h and isinstance(h[lk], (tuple, list)) and len(h[lk]) == 2:
+                        extracted[key] = str(h[lk][1])
+        except Exception:
+            pass
+
+        if extracted:
+            result["headers"] = extracted
+            if "Content-Type" in extracted:
+                result["content_type"] = extracted["Content-Type"]
+            if "Content-Encoding" in extracted:
+                result["content_encoding"] = extracted["Content-Encoding"]
+            if "Location" in extracted:
+                result["location"] = extracted["Location"]
+
         return result
 
     def restore(self, obj):
@@ -141,7 +166,7 @@ try_import_django(
 try_import_django(
     "django.http.response",
     ["HttpResponseBase"],
-    HttpResponseRedirectLocationHandler,
+    DjangoHttpResponseHeadersHandler,
     base=True,
 )
 
