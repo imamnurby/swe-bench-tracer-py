c9d16394781dcf30806e1e1ba120087cd01bf071
Zhiyuan Pan <zy_pan@zju.edu.cn>
2025-12-18 15:48:57 +0800

    add classes in sphinx

--- tracer/serializer/ext/sphinx.py
+++ tracer/serializer/ext/sphinx.py
@@ -1,13 +1,30 @@
 from functools import partial
 
+from jsonpickle.handlers import BaseHandler
 from tracer.serializer.ext.common import (
     PlainHandler,
     try_import,
     register_registry_handlers,
+    canonical_class_name,
 )
 
 try_import_sphinx = partial(try_import, registry='sphinx')
 register_handlers = partial(register_registry_handlers, registry='sphinx')
+
+class MessageHandler(BaseHandler):
+    def flatten(self, obj, data):
+        results = {"py/object": canonical_class_name(obj)}
+        try:
+            results.update({
+                "text": obj.text,
+                "locations": self.context.flatten(obj.locations),
+            })
+        except Exception:
+            pass
+        return results
+
+    def restore(self, obj):
+        return obj
 
 try_import_sphinx(
     "sphinx.testing.util",
@@ -20,4 +37,15 @@ try_import_sphinx(
     ["SphinxBaseReader"],
     PlainHandler,
     base=True,
+)
+try_import_sphinx(
+    "sphinx.parsers",
+    ["Parser"],
+    PlainHandler,
+    base=True,
+)
+try_import_sphinx(
+    "sphinx.builders.gettext",
+    ["Message"],
+    MessageHandler,
 )
