fb4592b36ed0bce4dde35b194eee854514c189ad
Zhiyuan Pan <zy_pan@zju.edu.cn>
2025-12-18 17:59:07 +0800

    update xarray handlers

--- tracer/serializer/ext/xarray.py
+++ tracer/serializer/ext/xarray.py
@@ -2,12 +2,51 @@ from functools import partial
 
 from jsonpickle.handlers import BaseHandler
 from tracer.serializer.ext.common import (
-    PlainHandler,
     try_import,
     register_registry_handlers,
     canonical_class_name,
 )
 
-# try_import_xarray = partial(try_import, registry='xarray')
-# register_handlers = partial(register_registry_handlers, registry='xarray')
+try_import_xarray = partial(try_import, registry='xarray')
+register_handlers = partial(register_registry_handlers, registry='xarray')
 
+from xarray import DataArray
+class VariableHandler(BaseHandler):
+    def flatten(self, obj, data):
+        results = {"py/object": canonical_class_name(obj)}
+        try:
+            results.update(self.context.flatten(obj.to_dict()))
+        except Exception:
+            pass
+        return results
+
+    def restore(self, obj):
+        return obj
+
+class DataArrayHandler(BaseHandler):
+    def flatten(self, obj: DataArray, data):
+        results = {"py/object": canonical_class_name(obj)}
+        try:
+            results.update(self.context.flatten({
+                "_variable": obj._variable,
+                "_coords": obj._coords,
+                "_name": obj._name,
+                "_indexes": obj._indexes,
+            }))
+        except Exception:
+            pass
+        return results
+
+    def restore(self, obj):
+        return obj
+
+try_import_xarray(
+    "xarray.core.variable",
+    ["Variable"],
+    VariableHandler,
+)
+try_import_xarray(
+    "xarray.core.dataarray",
+    ["DataArray"],
+    DataArrayHandler,
+)
