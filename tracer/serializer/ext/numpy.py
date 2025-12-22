f771f03678ff99999c87d7d08d001b741c9fb28a
Zhiyuan Pan <zy_pan@zju.edu.cn>
2025-12-23 02:17:12 +0800

    set customized ndarray handler for older version

--- tracer/serializer/ext/numpy.py
+++ tracer/serializer/ext/numpy.py
@@ -5,7 +5,11 @@ def register_handlers():
         try:
             jsonpickle_numpy.register_handlers(ndarray_mode='ignore', ndarray_size_threshold=None)
         except Exception:
+            from jsonpickle import register, unregister
+            ndarray_handler = jsonpickle_numpy.NumpyNDArrayHandlerView(mode='ignore', size_threshold=None)
             jsonpickle_numpy.register_handlers()
+            unregister(np.ndarray)
+            register(np.ndarray, ndarray_handler)
         return [
             np.ndarray, np.dtype, np.generic, np.dtype(np.void).__class__,
             np.dtype(np.float32).__class__, np.dtype(np.int32).__class__,
