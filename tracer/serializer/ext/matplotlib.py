d93a0645d1e63b0e8721477b1a31548c3cb684e0
Zhiyuan Pan <zy_pan@zju.edu.cn>
2026-01-09 23:49:31 +0800

    support matplotlib

---
+++ tracer/serializer/ext/matplotlib.py
@@ -0,0 +1,16 @@
+from functools import partial
+
+from tracer.serializer.ext.common import (
+    PlainHandler,
+    try_import,
+    register_registry_handlers,
+)
+
+try_import_matplotlib = partial(try_import, registry='matplotlib')
+register_handlers = partial(register_registry_handlers, registry='matplotlib')
+
+try_import_matplotlib(
+    "matplotlib.transforms",
+    ["Bbox"],
+    PlainHandler,
+)
